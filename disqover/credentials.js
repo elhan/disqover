00a187114a9226c6a7073b7a6a4921305f04f41f
KPouianou <kpouianou@gmail.com>
2013-05-29 21:24:35 +0300

    credentials

---
+++ disqover/credentials.js
@@ -0,0 +1,7 @@
+var credentials = {
+    api_secret : '2LKzK0WD65VEP8FVHbwenEOA8lQQWHm1R1xOcjKgXeMLQ21Rb5HIVWv18bHCyHcr',
+    api_key : 'dzjajujpZLi6sZNCQ4Zcfj5DBXMwkKcDnsJkFL5fSCzVxMCirS2uACFwB07uQPbi',
+    access_token : '10773cc55e6d449facab34cc1df47b4a'
+};
+
+module.exports = credentials;
